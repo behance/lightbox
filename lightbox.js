347b9ce7b21d15c590750834399af631b64d6008
Yuriy Nemtsov <nemtsov@gmail.com>
2016-09-22 19:12:21 -0400

    ES6 refactor

--- lightbox.js
+++ lightbox.js
@@ -2,85 +2,87 @@ import $ from 'jquery';
 import tinycolor from 'tinycolor2';
 import 'style!./sass/lightbox.scss';
 
-var bgColor,
-    opacity,
-    $blocking = $('<div class="js-blocking" id="lightbox-blocking"></div>'),
-    $body = $(document.body),
-    template = `
-      <div class="js-lightbox-wrap" id="lightbox-wrap">
-        <div class="js-lightbox-inner-wrap" id="lightbox-inner-wrap">
-          <div class="js-img-wrap" id="lightbox-img-wrap">
-            <div class="control prev js-control js-prev">
-              <svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0" y="0" width="60" height="60" viewBox="0 0 60 60" xml:space="preserve">
-                <circle class="lightbox-icon-bg" cx="30" cy="30" r="30"/>
-                <path class="lightbox-icon-arrow" d="M28.6 30l7.9-7.9c0.5-0.5 0.7-1.1 0.7-1.7s-0.2-1.2-0.7-1.7c-0.9-0.9-2.5-0.9-3.4 0l-9.6 9.6c-0.5 0.5-0.7 1.1-0.7 1.7s0.2 1.2 0.7 1.7l9.6 9.6c0.9 0.9 2.5 0.9 3.4 0 0.5-0.5 0.7-1.1 0.7-1.7 0-0.6-0.2-1.2-0.7-1.7L28.6 30z"/>
-              </svg>
-            </div>
-            <div class="control next js-control js-next">
-              <svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0" y="0" width="60" height="60" viewBox="0 0 60 60" xml:space="preserve">
-                <circle class="lightbox-icon-bg" cx="30" cy="30" r="30"/>
-                <path class="lightbox-icon-arrow" d="M31.4 30l-7.9 7.9c-0.5 0.5-0.7 1.1-0.7 1.7 0 0.6 0.2 1.2 0.7 1.7 0.9 0.9 2.5 0.9 3.4 0l9.6-9.6c0.5-0.5 0.7-1.1 0.7-1.7s-0.2-1.2-0.7-1.7l-9.6-9.6c-0.9-0.9-2.5-0.9-3.4 0 -0.5 0.5-0.7 1.1-0.7 1.7 0 0.6 0.2 1.2 0.7 1.7L31.4 30z"/>
-              </svg>
-            </div>
-            <div class="control close js-control js-close">
-              <svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0" y="0" viewBox="0 0 26 26" preserveAspectRatio="xMidYMid meet">
-                <rect x="-3.4" y="11" transform="matrix(0.7071 0.7071 -0.7071 0.7071 13 -5.3848)" class="st0" width="32.7" height="4" />
-                <rect x="-3.4" y="11" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -5.3848 13)" class="st0" width="32.7" height="4" />
-              </svg>
-            </div>
-            <img/>
-          </div>
+const ESCAPE_KEYCODE = 27;
+const LEFT_ARROW_KEYCODE = 37;
+const RIGHT_ARROW_KEYCODE = 39;
+const FADE_TIME = 200;
+const $blocking = $('<div class="js-blocking" id="lightbox-blocking"></div>');
+const $body = $(document.body);
+const images = [];
+const template = `
+  <div class="js-lightbox-wrap" id="lightbox-wrap">
+    <div class="js-lightbox-inner-wrap" id="lightbox-inner-wrap">
+      <div class="js-img-wrap" id="lightbox-img-wrap">
+        <div class="control prev js-control js-prev">
+          <svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0" y="0" width="60" height="60" viewBox="0 0 60 60" xml:space="preserve">
+            <circle class="lightbox-icon-bg" cx="30" cy="30" r="30"/>
+            <path class="lightbox-icon-arrow" d="M28.6 30l7.9-7.9c0.5-0.5 0.7-1.1 0.7-1.7s-0.2-1.2-0.7-1.7c-0.9-0.9-2.5-0.9-3.4 0l-9.6 9.6c-0.5 0.5-0.7 1.1-0.7 1.7s0.2 1.2 0.7 1.7l9.6 9.6c0.9 0.9 2.5 0.9 3.4 0 0.5-0.5 0.7-1.1 0.7-1.7 0-0.6-0.2-1.2-0.7-1.7L28.6 30z"/>
+          </svg>
         </div>
+        <div class="control next js-control js-next">
+          <svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0" y="0" width="60" height="60" viewBox="0 0 60 60" xml:space="preserve">
+            <circle class="lightbox-icon-bg" cx="30" cy="30" r="30"/>
+            <path class="lightbox-icon-arrow" d="M31.4 30l-7.9 7.9c-0.5 0.5-0.7 1.1-0.7 1.7 0 0.6 0.2 1.2 0.7 1.7 0.9 0.9 2.5 0.9 3.4 0l9.6-9.6c0.5-0.5 0.7-1.1 0.7-1.7s-0.2-1.2-0.7-1.7l-9.6-9.6c-0.9-0.9-2.5-0.9-3.4 0 -0.5 0.5-0.7 1.1-0.7 1.7 0 0.6 0.2 1.2 0.7 1.7L31.4 30z"/>
+          </svg>
+        </div>
+        <div class="control close js-control js-close">
+          <svg xmlns="http://www.w3.org/2000/svg" version="1.1" x="0" y="0" viewBox="0 0 26 26" preserveAspectRatio="xMidYMid meet">
+            <rect x="-3.4" y="11" transform="matrix(0.7071 0.7071 -0.7071 0.7071 13 -5.3848)" class="st0" width="32.7" height="4" />
+            <rect x="-3.4" y="11" transform="matrix(0.7071 -0.7071 0.7071 0.7071 -5.3848 13)" class="st0" width="32.7" height="4" />
+          </svg>
+        </div>
+        <img/>
       </div>
-    `,
-    images = [],
-    active = false,
-    loading = false,
-    FADE_TIME = 200,
-    single = false,
-    $context,
-    $activeImage;
+    </div>
+  </div>
+`;
 
-var LightboxImage = function(src, id) {
-  this.src = src;
-  this.id = id;
-  this.$view = $(template).hide();
+let active = false;
+let loading = false;
+let bgColor;
+let opacity;
+let single = false;
+let $context;
+let $activeImage;
 
-  var self = this;
+class LightboxImage {
+  constructor(src, id) {
+    this.src = src;
+    this.id = id;
+    this.$view = $(template).hide();
+  }
 
-  this.imgLoad = function($loadedImg) {
+  imgLoad($loadedImg) {
     var top;
 
     $body.removeClass('lightbox-loading');
-
-    $body.append(self.$view);
-
-    self.$view.fadeIn(FADE_TIME);
+    $body.append(this.$view);
+    this.$view.fadeIn(FADE_TIME);
 
     if ($activeImage) {
-      $activeImage.fadeOut(FADE_TIME, function() {
+      $activeImage.fadeOut(FADE_TIME, () => {
         $activeImage.remove();
-        $activeImage = self.$view;
+        $activeImage = this.$view;
         loading = false;
       });
     }
     else {
-      $activeImage = self.$view;
+      $activeImage = this.$view;
       loading = false;
     }
 
-    active = self.id;
+    active = this.id;
     if ($loadedImg.height()) {
       top = ($(window).height() - $loadedImg.height()) / 2;
-      self.$view.addClass('shown');
-      self.$view.css('top', top);
-      self.$view.find('.js-img-wrap').height($loadedImg.height());
+      this.$view.addClass('shown');
+      this.$view.css('top', top);
+      this.$view.find('.js-img-wrap').height($loadedImg.height());
     }
 
-    _setCloseIconColor(self.$view, bgColor);
-  };
+    _setCloseIconColor(this.$view, bgColor);
+  }
 
-  this.render = function() {
+  render() {
     if (loading) {
       return;
     }
@@ -110,11 +112,11 @@ var LightboxImage = function(src, id) {
       this.imgLoad($img);
     }
     else {
-      $img.on('load', function() {
+      $img.on('load', () => {
         this.imgLoad($img);
-      }.bind(this));
+      });
     }
-  };
+  }
 };
 
 function close() {
@@ -146,7 +148,7 @@ function bind() {
   });
 
   $(document).on('keyup.lightbox', function(e) {
-    if (e.keyCode === 27) { // escape
+    if (e.keyCode === ESCAPE_KEYCODE) {
       close();
     }
   });
@@ -166,10 +168,10 @@ function bind() {
   });
 
   $(document).on('keyup.lightbox', function(e) {
-    if (e.keyCode === 37) { // left arrow
+    if (e.keyCode === LEFT_ARROW_KEYCODE) {
       prev();
     }
-    else if (e.keyCode === 39) { // right arrow
+    else if (e.keyCode === RIGHT_ARROW_KEYCODE) {
       next();
     }
   });
@@ -188,21 +190,20 @@ function _setCloseIconColor($context, bgColor) {
 }
 
 function init(options) {
-  var defaults = {
+  const config = $.extend({
     context: document.body,
     imageSelector: '.js-lightbox',
     imageSrcDataAttr: 'src',
     bgColor: '#fff',
     opacity: '0.94'
-  };
-  var config = $.extend(defaults, options);
+  }, options);
 
   $context = $(config.context);
   bgColor = config.bgColor;
   opacity = config.opacity;
 
-  $context.find(config.imageSelector).each(function(i, el) {
-    var $img = $(el);
+  $context.find(config.imageSelector).each((i, el) => {
+    const $img = $(el);
     $img.data('img-id', i).addClass('lightbox-link');
     images[i] = new LightboxImage($img.data(config.imageSrcDataAttr), i);
   });
